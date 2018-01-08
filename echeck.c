dcece2334ca6d0999b3f271d590fcdcd2844a0c3
Steffen Mecke <stm2@users.sf.net>
2018-01-08 01:36:25 +0100

    fix braces

--- echeck.c
+++ echeck.c
@@ -4693,9 +4693,10 @@ int check_options(int argc, char *argv[], char dostop, char command_line)
                 ("Leere Pfad-Angabe ungültig\nEmpty path invalid\n", stderr);
               exit(1);
             }
-          } else /* -Ppath */ if (*(argv[i] + 2))
+          } else /* -Ppath */ if (*(argv[i] + 2)) {
             free(path);
             path = strdup((char *)(argv[i] + 2));
+          }
         }
         break;
 
