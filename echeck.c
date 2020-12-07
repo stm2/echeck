6f1ef395ab0e357a7a2bee7e522e9d85b7530fc5
Enno Rehling <enno.rehling@gmail.com>
2020-12-07 21:59:27 +0100

    increase version number

--- echeck.c
+++ echeck.c
@@ -87,7 +87,7 @@ int AddTestSuites(CuSuite * suite, const char *names);
 
 #include <string.h>
 
-static const char *echeck_version = "4.5.5";
+static const char *echeck_version = "4.5.6";
 
 #define DEFAULT_PATH "."
 
